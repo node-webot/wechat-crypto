711c090004036acf8d8be4e268476c062abfa2bd
Jackson Tian <shyvo1987@gmail.com>
2014-11-04 14:45:55 +0800

    Extract wechat crypto

---
+++ test/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  corpid: 'wx1570bcc0b3cf8bd0',
+  corpsecret: 'yy3S_IBbELpalg9HJbbWj3wnWtc-u8vwz0lT7x2fI80UKAHKdxZtS8IyYc-7jdeA',
+  token: 'RMNlACHlV5ThzfRlVS4D4',
+  encodingAESKey: 'bAZC8S8nz5fcQ0Ui2HxQVGbC6FzyrzWKSuDbmzpBo3G'
+};
